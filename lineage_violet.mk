9488fd35b54c07d9a3536cd331d4f23798b4fe19
Joker-V2 <nipinna0@gmail.com>
2025-08-11 06:19:14 +0000

    violet: Axion-ify

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -15,6 +15,14 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# AxionAOSP
+AXION_CAMERA_REAR_INFO := 48,5
+AXION_CAMERA_FRONT_INFO := 13
+AXION_MAINTAINER := Joker-V2
+AXION_PROCESSOR := Qualcomm_Snapdragon_675
+BYPASS_CHARGE_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
 PRODUCT_DEVICE := violet
